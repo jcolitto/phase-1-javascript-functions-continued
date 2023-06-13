d873fd91c5a6f0decbae70930e0c8757b7e3ad65
Joseph <jcolitto@mac.com>
2023-06-13 16:52:51 -0500

    Need to revisit this one too

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(sat = "roller-skate") {
+  return `This Saturday, I want to ${sat}!`;
+}
+
+function mondayWork(mon = "go to the office") {
+  return `This Monday, I will ${mon}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));
